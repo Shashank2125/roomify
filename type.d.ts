0e2f5faf283280a9bed05663664164933b9fdd2a
Shashank2125 <138694950+Shashank2125@users.noreply.github.com>
2026-04-03 20:53:41 +0530

    First commit

---
+++ type.d.ts
@@ -0,0 +1,16 @@
+interface AuthState{
+    isSignedIn: boolean;
+    username: string | null;
+    userId: string | null;
+
+}
+type AuthContext = {
+    isSignedIn: boolean;
+    username: string | null;
+    userId: string | null;
+    refreshAuth:()=>Promise<boolean>;
+    signIn:()=>Promise<boolean>;
+    signOut:()=>Promise<boolean>
+
+
+}
