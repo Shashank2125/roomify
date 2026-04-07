68670e3ccb67119c434711e27c4d73fc5f1f2db5
Shashank2125 <138694950+Shashank2125@users.noreply.github.com>
2026-04-07 21:58:59 +0530

    upload.tsx parltial implement

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,7 @@ import type { Route } from "./+types/home";
 import Navbar from "../../components/Navbar";
 import {ArrowRight, ArrowUpRight, Clock, Layers} from "lucide-react";
 import Button from "../../components/ui/Button";
+import Upload from "../../components/upload";
 
 
 export function meta({}: Route.MetaArgs) {
@@ -51,7 +52,7 @@ export default function Home() {
                       <h3>Upload your floor plan</h3>
                       <p>Support JPG, PNG, formats up to 10MB </p>
                   </div>
-                  <p>Upload images</p>
+                  <Upload/>
               </div>
 
           </div>
